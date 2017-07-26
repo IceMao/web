408adc45f83f43e08ea456881ff643bc4c92676a
icemao <18845294217@163.com>
2017-07-26 14:08:16 +0800

    init

--- app/index.js
+++ app/index.js
@@ -1,12 +1,37 @@
-// import Vue from "vue";
-// import vueRouter from 'vue-router';
+/**
+ * Created by ice on 2017/7/20.
+ */
+import Vue from "vue";
+import vueRouter from 'vue-router';
+import vueResource from 'vue-resource';
 
-// Vue.use(vueRouter);
+Vue.use(vueRouter);
+Vue.use(vueResource);
 
-// const NotFound = { template: '<h2>NotFound </h2>' }
-// const Home = { template: '<p>我是谁！！！</p>' }
-// const About = { template: '<h1>About </h1>' }
+const NotFound = { template: '<h2>NotFound </h2>' }
+const Home = { template: '<p>我是谁！！！</p>' }
+const About = { template: '<h1>About </h1>' }
 
 require('./common/styles/common.css');
 
-console.info(42343234,"icIc")
+//路由
+const routes = [{
+    path: '/home',
+    component: Home
+},{
+    path: '/about',
+    component: About
+},{
+    path: '/not',
+    component: NotFound
+}];
+
+const router = new vueRouter({
+  hashbang: false,
+  mode: 'history',
+    routes
+});
+
+const app = new Vue({
+    router,
+}).$mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,14 @@
  * Created by ice on 2017/7/20.
  */
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const fs = require('fs');
+
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
+require('babel-register')(babelrc);
 
 //判断当前运行环境是开发模式还是生产模式
 const nodeEnv = process.env.NODE_ENV || 'development';
@@ -12,29 +18,11 @@ const isPro = nodeEnv === 'production';
 const appPath = path.resolve(__dirname, './app/');
 const tmplPath = path.resolve(__dirname, './app/tmpl');
 
-if (isPro) {
-    plugins.push(
-        new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV': JSON.stringify(nodeEnv)
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    )
-} else {
-    plugins.push(
-        new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV': JSON.stringify(nodeEnv)
-            }
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    )
-}
+console.log('当前运行环境：', isPro ? 'production' : 'development');
+
+var resolve = function (dir) {
+    return path.join(__dirname, dir);
+};
 
 var plugins = [
     new ExtractTextPlugin('[name].css'),
@@ -46,52 +34,94 @@ var plugins = [
         chunks: ['index'],
         hash: true,
         cache: false
-    })
+    }),
+    new webpack.DefinePlugin({
+        'process.env':{
+            'NODE_ENV': JSON.stringify(nodeEnv)
+        }
+    }),
 ];
+if (isPro) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    )
+} else {
+    plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    )
+}
 
 module.exports = {
+    devtool: !isPro && 'cheap-eval-source-map',
     entry: {
         index: path.resolve(appPath, 'index.js')
     },
     output: {
-        filename:'[name].js',
-        path: path.resolve(appPath, 'dist'),
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
         publicPath: isPro ? './' : '/',
         chunkFilename: '[name].[hash].js'
     },
+    plugins: plugins,
+    node: {
+        fs: 'empty'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.css'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.join(appPath)
+        ],
+        alias: {
+            'static': path.join(appPath, 'common'),
+            'components': path.join(appPath, 'views/components'),
+            'vue': path.join(__dirname, '/node_modules/vue/dist/vue')
+        }
+    },
     module: {
-        rules:[{
-            test: /\.css$/,
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/,
+            include: resolve('/')
+        },{
+            test: /\.vue$/,
+            use: ['vue-loader'],
+            exclude: /node_modules/,
+            include: resolve('views')
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            use: ['url-loader?limit=10000&name=files/[name].[hash:7].[ext]']
+        }, {
+            test: /\.(css)$/,
             use: ExtractTextPlugin.extract({
                 use: ['css-loader'],
                 fallback: 'style-loader'
             })
-        },{
-            test: /\.vue$/,
-            use: ['vue-loader'],
-            exclude: /node-modules/
-            // include: resolve('src')
-        }, {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            use: ['url-loader?limit=10000&name=files/[name].[hash:7].[ext]']
         }]
     },
-    plugins: plugins,
-    resolve: {
-        extensions: ['.js','.css','.vue'],
-        modules:[
-            path.resolve(__dirname, 'node-modules'),
-            path.join(appPath)
-        ],
-        alias:{
-            'static': path.join(appPath, 'common'),
-            'components': path.join(appPath, 'components')
-        }
-    },
     devServer: {
-        port: 3423,
+        contentBase: resolve('/'),
+        historyApiFallback: true,
+        compress: true,
+        port: 1234,
         hot: true,
         inline: true,
-        historyApiFallback: true,
+        stats: {
+            assets: true,
+            children: false,
+            modules: false,
+            chunks: false,
+            publicPath: false,
+            timings: true,
+            warnings: true,
+            colors: {
+                green: '\u001b[32m',
+            }
+        },
     }
-}; 
+};
